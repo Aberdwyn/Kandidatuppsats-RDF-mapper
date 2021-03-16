2fc0f2e4af7763a013168ab39ac69616cf340b60
Aberdwyn <Anti_012@hotmail.com>
2021-03-16 09:25:44 +0100

    data-generator now also creates a reader

--- graphql-server/modules/data-generator/generate-data.py
+++ graphql-server/modules/data-generator/generate-data.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 import random
 import sys 
-surnames1 = ["John", "Brandon", "Robert"]
-lastnames1 = ["Tolkien", "Sanderson", "Jordan"]
-surnames = ["John", "Brandon", "Robert", "Terry", "Patrick", "Joe", "Ursula", "Lewis", "David", "Neil", "Robin", "Andrzej", "Anne", "Rick", "Raymond", "George", "Steven", "Roger", "Margaret", "Janny", "Madeleine", "Guy", "Mercedes", "Diana", "Scott", "Tamora", "Cassanra", "Michael", "Cornelia", "Katharine", "Susan", "Lois", "Mark", "Trudi", "Victoria", "Sarah", "Janet", "Madeline", "Tad", "Laini", "Holly", "Gail", "Patricia", "Alix"]
-lastnames = ["Tolkien", "Sanderson", "Jordan", "Pratchet", "Rothfuss", "Abercrombie", "Le Guin", "Caroll", "Eddings", "Gaiman", "Hobb", "Sapkowski", "McCaffrey", "Riordan", "Brooks", "Feist", "Martin", "Erikson", "Zelazny", "Weis", "Wurts", "L'Engle", "Kay", "Lackey", "Jones", "Lynch", "Pierce", "Clare", "Sullivan", "Funke", "Kerr", "Cooper", "Bujold", "Lawrence", "Canavan", "Schwab", "Maas", "Arden", "Morris", "Jemisin", "Williams", "Taylor", "Black", "Carriger", "Addison", "McKillip", "Harrow"]
+
+author_surnames = ["John", "Brandon", "Robert", "Terry", "Patrick", "Joe", "Ursula", "Lewis", "David", "Neil", "Robin", "Andrzej", "Anne", "Rick", "Raymond", "George", "Steven", "Roger", "Margaret", "Janny", "Madeleine", "Guy", "Mercedes", "Diana", "Scott", "Tamora", "Cassanra", "Michael", "Cornelia", "Katharine", "Susan", "Lois", "Mark", "Trudi", "Victoria", "Sarah", "Janet", "Madeline", "Tad", "Laini", "Holly", "Gail", "Patricia", "Alix"]
+author_lastnames = ["Tolkien", "Sanderson", "Jordan", "Pratchet", "Rothfuss", "Abercrombie", "Le Guin", "Caroll", "Eddings", "Gaiman", "Hobb", "Sapkowski", "McCaffrey", "Riordan", "Brooks", "Feist", "Martin", "Erikson", "Zelazny", "Weis", "Wurts", "L'Engle", "Kay", "Lackey", "Jones", "Lynch", "Pierce", "Clare", "Sullivan", "Funke", "Kerr", "Cooper", "Bujold", "Lawrence", "Canavan", "Schwab", "Maas", "Arden", "Morris", "Jemisin", "Williams", "Taylor", "Black", "Carriger", "Addison", "McKillip", "Harrow"]
 def generate_author():
     """Returns a string of a surname and a lastname fitting for an author randomly picked from pre-defined lists of surnames and lastnames."""
-    return surnames[random.randint(0, len(surnames) - 1)] + " " + lastnames[random.randint(0, len(surnames) - 1)]
+    return author_surnames[random.randint(0, len(author_surnames) - 1)] + " " + author_lastnames[random.randint(0, len(author_surnames) - 1)]
 
 
 fantasy_noun1 = ["Company"]
 fantasy_adj1 = ["Golden"]
-fantasy_adj = ["Golden", "Black", "White", "Dark", "Holy", "Final", "Warded", "Neverending", "Endless", "Enormous", "Magical", "Crystal", "Last", "Wild", "Diamond", "Lost", "Great", "Shining", "Deadly", "Evil", "Mysterious", "Dragonbone", "Radiant", "Glimmering"]
-fantasy_noun = ["Company", "Lord", "Rule", "Empire", "King", "Queen", "Prince", "Princess", "Prism", "Story", "Tale", "Ship", "Journey", "Army", "Horde", "Theft", "Sword", "Storm", "Staff", "Wheel", "Cave", "Wish", "Unicorn", "Dragon", "Apprentice", "Pawn", "Assassin", "Ranger", "Paladin", "Cleric", "Wizard", "Quest", "Oath", "Errand", "Throne", "Chair", "Curse", "Crown", "Way", "Name", "Game", "City", "Garden"]
+fantasy_adj = ["Golden", "Black", "White", "Dark", "Holy", "Final", "Warded", "Neverending", "Endless", "Flowing", "Steaming", "Enormous", "Magical", "Crystal", "Last", "Wild", "Diamond", "Lost", "Great", "Shining", "Deadly", "Evil", "Mysterious", "Dragonbone", "Radiant", "Glimmering"]
+fantasy_noun = ["Company", "Lord", "Rule", "Horseman", "Empire", "King", "Queen", "Prince", "Princess", "Prism", "Story", "Tale", "Ship", "Journey", "Army", "Horde", "Theft", "Sword", "Storm", "Staff", "Wheel", "Cave", "Wish", "Unicorn", "Dragon", "Apprentice", "Pawn", "Assassin", "Ranger", "Paladin", "Cleric", "Wizard", "Quest", "Oath", "Errand", "Throne", "Chair", "Curse", "Crown", "Way", "Name", "Game", "City", "Garden"]
 fantasy_genetive = ["Magic's", "Assassin's", "King's", "Queen's", "Wizard's", "Knight's", "Steven's", "Fool's", "Hero's"]
 fantasy_what_is_this = ["Darkness", "Chaos", "Death", "Plague", "Kings", "Swords", "Time", "Magic", "the Forest", "Radiance", "the Moon", "the World", "Wind", "Flame", "the Stars", "Thrones", "Glass", "Thorns", "the Empire", "Blood", "the Queen", "Ages", "Shadows"]
 def generate_book_title():
     """Returns a string title."""
-    d20 = random.randint(0, 20)
+    d20 = random.randint(1, 20)
     if d20 <= 4:
         #genetive noun
          return  fantasy_genetive[random.randint(0, len(fantasy_genetive) - 1)] + " " + fantasy_noun[random.randint(0, len(fantasy_noun) - 1)]
@@ -31,6 +30,7 @@ def generate_book_title():
 
 
 books = {}
+book_ids = {}
 def generate_books(nr):
     """Generate a number of books equal to nr and saves them in a dictionary."""
     for n in range(int(nr)):
@@ -43,6 +43,10 @@ def generate_books(nr):
             books[book] += [author]
         else:
             books[book] = [author]
+    counter = 1
+    for book in books:
+        book_ids[book] = f'Book/{counter}'
+        counter += 1
 
 
 authors = {}
@@ -65,6 +69,42 @@ def find_relations_among_authors():
                 known_relations[books[book][i]] = books[book][:i] + books[book][i+1:]
 
 
+reader_surnames = ["Bob", "Bobby", "Alfred", "Eva", "Adam", "Christina", "Sofia", "Anton", "Martin", "Magdalena", "Olivia", "Emma", "Amelia", "Ava", "Noah", "Liam", "Oliver", "Elijah", "Isabella", "Mia", "Charlotte", "Gianna", "Luna", "Aria", "Harper", "Ella", "Evelyn", "Mila", "Ellie", "Scarlett", "Lily", "Elizabeth", "Hazel", "Mohammed", "Alexander", "Samuel", "Henry", "Ezra", "Julian", "Michael", "Christopher", "Daniel", "Sebastian", "William", "Lucas"]
+reader_lastnames = ["Nilsson", "Andersson", "Johansson", "Karlsson", "Larsson", "Olsson", "Persson", "Svensson", "Gustafsson", "Petterson", "Jonsson", "Jansson", "Hansson", "Smith", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Campbell", "Macdonald", "Murray"]
+def generate_reader_name():
+    """Generate and return a reader name."""
+    return reader_surnames[random.randint(0, len(reader_surnames) - 1)] + " " + reader_lastnames[random.randint(0, len(reader_lastnames) - 1)]
+
+readers = {}
+def generate_reader(n):
+    """Generate and store n Readers."""
+    counter = 1
+    for i in range(n):
+        name = generate_reader_name()
+        if not name in readers:
+            readers[name] = f'Reader/{counter}'
+            counter += 1
+
+reader_favourite_book = {}
+reader_knows = {}
+def find_reader_relations():
+    """Find and store reader relations"""
+    for reader in readers:
+        d100 = random.randint(1, 100)
+        if d100 <= 50:
+            reader_favourite_book[readers[reader]] = random.choice(list(book_ids.values()))
+
+        d100 = random.randint(1, 100)
+        if d100 <= 5:
+            #TODO: fix so that you cannot be friend of yourself
+            reader_knows[readers[reader]] = [random.choice(list(authors.values()))] + [random.choice(list(readers.values()))]
+        elif d100 > 5 and d100 <= 10:
+            reader_knows[readers[reader]] = [random.choice(list(authors.values()))]
+        elif d100 > 10 and d100 <= 25:
+            reader_knows[readers[reader]] = [random.choice(list(readers.values()))] + [random.choice(list(readers.values()))]
+        elif d100 > 25 and d100 <= 50:
+            reader_knows[readers[reader]] = [random.choice(list(readers.values()))]
+
 
 def write_data_to_file():
     """Writes the generated data to a file."""
@@ -74,23 +114,54 @@ def write_data_to_file():
         
         #books
         file.write('    "Book": {\n')
-        book_counter = 1
         for book in books:
-            file.write(f'        "Book/{book_counter}": {{ id: "Book/{book_counter}", title: "{book}", authors: [')
+            file.write(f'        "{book_ids[book]}": {{ id: "{book_ids[book]}", title: "{book}", authors: [')
             for author in books[book]:
                 file.write(f'{{"id": "{authors[author]}"}}, ')
             file.write(']},\n')
-            book_counter += 1
         file.write('    },\n')
         
         #authors
         file.write('    "Author": {\n')
         for author in authors:
-            file.write(f'       "{authors[author]}": {{ id: "{authors[author]}", name: "{author}", "knows": [')
+            file.write(f'       "{authors[author]}": {{ id: "{authors[author]}", name: "{author}"')
             if author in known_relations:
+                file.write(', "knows": [')
                 for associate in known_relations[author]:
                     file.write(f' {{ id: "{authors[associate]}" }},')
-            file.write(' ], description: "" },\n')
+                file.write(' ]')
+            else:
+                #some small chance that the authors's knows value is an empty list
+                d20 = random.randint(1, 20)
+                if d20 < 5:
+                    file.write(', "knows": []')
+            file.write(', description: "" },\n')
+        file.write('    },\n')
+
+        #readers
+        file.write('    "Reader": {\n')
+        for reader in readers:
+            file.write(f'       "{readers[reader]}": {{ id: "{readers[reader]}", name: "{reader}"')
+            #reader knows
+            if readers[reader] in reader_knows:
+                file.write(f', "knows": [')
+                for associate in reader_knows[readers[reader]]:
+                    file.write(f' {{ id: "{associate}" }},')
+                file.write(' ]')
+            else:
+                #some small chance that the reader's knows value is an empty list
+                d20 = random.randint(1, 20)
+                if d20 < 5:
+                    file.write(', "knows": []')
+            #reader's favourite book
+            if readers[reader] in reader_favourite_book:
+                file.write(f', "favourite_book": {{id: "{reader_favourite_book[readers[reader]]}"}}')
+            else:
+                #some small chance that the reader's favourite book value is an empty string
+                d20 = random.randint(1, 20)
+                if d20 < 5:
+                    file.write(', "favourite_book": ""')
+            file.write(', },\n')
         file.write('    },\n')
 
         #end
@@ -104,7 +175,11 @@ if __name__ == '__main__':
     generate_books(1000)
     generate_id_for_authors()
     find_relations_among_authors()
+
+    generate_reader(500)
+    find_reader_relations()
     write_data_to_file()
+
 
     """
     try: 
