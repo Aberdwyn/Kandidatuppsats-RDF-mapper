865b5b91e2fd283d71e401b921b297b346ecd4c2
Robin Keskisärkkä <robin.keski@gmail.com>
2021-04-21 14:11:13 +0200

    added comments in code

--- graphql-to-rdf-mapper/testing/directives-utils.py
+++ graphql-to-rdf-mapper/testing/directives-utils.py
@@ -12,6 +12,8 @@ from graphql.language.parser import parse
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 import copy
+from mako.template import Template
+import re
 
 def decapitalize(string: str):
     """
@@ -337,11 +339,42 @@ def apply_join(collections, graphql_mapping_schema):
 
     return results
 
+def apply_rdf_template(filename, results):
+    """Transform a provided RDF mapping schema into a valid Mako template and apply results.
+    
+    This is a very simple translation that doesn't offer much in terms of customization. But basically,
+    it holds to the initial idea of keeping the RDF mapping file/template format very simple. E.g.:
+    <Person/{Person.id} a :Person ;
+         :name "{Person.name}" .
+
+    where Person.id and Person.name refer to field in the GraphQL schema mapping. Since this is not a valid
+    format that can easily be interpretted by anu existing template system this function applies a transformation
+    on the mapping to produce a valide Mako template compatible with the format we get as results.
+
+    Not supported:
+    - Variable list fields: Do we need to add conditionals to the generated tmeplate for this?
+    """
+    rdf_mapping_schema = open(filename, "r").read()
+
+    # Rename all variables according to the 'collection_field' format
+    # Pass a function instead of a pattern to sub()
+    f = lambda p: f"${{result['{get_collection_name(p.group(1))}_{p.group(2)}']}}"
+    s = re.sub(r'\{(.+)\.(.+)\}', f, rdf_mapping_schema)
+    
+    template_string = "% for result in data:\n" + s + "\n% endfor"
+    # Check out the generate template
+    print("Template string:")
+    print(template_string)
+    print()
+    
+    template = Template(template_string)
+    return template.render(data=results)
+
 def main():
     graphql_endpoint_url = "http://localhost:4000/"
     
     # Generate query
-    graphql_mapping_schema = load_schema('./author_schema.graphql')
+    graphql_mapping_schema = load_schema('./graphql-mapping-schema.graphql')
     rdf_template = ''
 
     query = build_graphql_query(graphql_mapping_schema)
@@ -355,8 +388,16 @@ def main():
     # WARNING: If there exists no valid join key between two collections this is equivalent to producing a cross-product.
     results = apply_join(collections, graphql_mapping_schema)
     print("Result size:", len(results))
-    print("Print top 10 results:")
+    print("Print the first 10 results")
     print(results[:10])
+    print()
+    
+    # Generate RDF
+    # apply template
+    rdf = apply_rdf_template("rdf-mapping-schema.txt", results)
+    print("Generated RDF:")
+    print(rdf)
+    
 
 if __name__ == '__main__':
     main()
