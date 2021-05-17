613bb82c3fd8515398d379adb7b26af5e9b4014a
Aberdwyn <Anti_012@hotmail.com>
2021-05-17 14:39:11 +0200

    code cleanup

--- graphql-to-rdf-mapper/modules/rdf_utils.py
+++ graphql-to-rdf-mapper/modules/rdf_utils.py
@@ -9,18 +9,22 @@ def apply_rdf_template(filename, results):
     
     This is a very simple translation that doesn't offer much in terms of customization. But basically,
     it holds to the initial idea of keeping the RDF mapping file/template format very simple. E.g.:
-    <Person/{Person.id} a :Person ;
-         :name "{Person.name}" .
+    <Person/{Person.id} a :Person .
+    <Person/{Person.id} :name "{Person.name}" .
 
     where Person.id and Person.name refer to field in the GraphQL schema mapping. Since this is not a valid
     format that can easily be interpretted by anu existing template system this function applies a transformation
-    on the mapping to produce a valide Mako template compatible with the format we get as results.
+    on the mapping to produce a valid Mako template compatible with the format we get as results.
+
+    Because we use union when there are no valid join keys to create the result, 
+    we cannot guarantee that all data in the results looks the same.
+    The template handles this by for each triple the rdf mapping schema wants to create,
+    check if the current triple is possible to create and in that case do it, otherwise skip
 
     Not supported:
     - Variable list fields: Do we need to add conditionals to the generated tmeplate for this?
     """
     rdf_mapping_schema = open(filename, "r").read()
-    #print(re.findall(r'\{(.*?)\.(.*?)\}', rdf_mapping_schema))
 
     # Rename all variables according to the 'collection_field' format
     # Pass a function instead of a pattern to sub()
@@ -39,32 +43,15 @@ def apply_rdf_template(filename, results):
 
     #split triples
     s = re.findall(r'\<.*? \.', s)
-    print(s)
-    
-
-    #template_string = "% for result in data:\n" 
-    #for triple in s:
-    #    data = re.findall(r"\['(.*?)'\]", triple)
-    #    print(data)
-    #    template_string += "    <% \n"
-    #    template_string += "        all_keys_exist = True \n"
-    #    template_string += "            for key in data: \n"
-    #    template_string += "                if not key in result: \n"
-    #    template_string += "                    all_keys_exist = False  \n"
-    #    template_string += "    %> \n"
-    #    template_string += "    %if all_keys_exist: \n"
-    #    template_string += "        " + triple + "\n"
-    #    template_string += "    %endif \n"
-    #template_string += "%endfor"
-    
-
-
-
+    #print(s)
+   
     template_string = "% for result in data:\n" 
     for triple in s:
+        #find all references to result
         data = re.findall(r"\['(.*?)'\]", triple)
-        print(data)
-        #could probably look nicer with string formating
+        #print(data)
+        #check if all references to result exists as keys in results, 
+        #only try to create this triple for this result if they do
         template_string += "    % if set(" + str(data) + ").issubset(set(result.keys())): \n"
         template_string += "        " + triple + "\n"
         template_string += "    % endif\n"
@@ -74,10 +61,6 @@ def apply_rdf_template(filename, results):
     print(template_string)
     print()
 
-    
-    
-
-    
     template = Template(template_string)
     rdf_data = template.render(data=results)
 
@@ -85,14 +68,11 @@ def apply_rdf_template(filename, results):
         file.write(prefixes)
         file.write(rdf_data)
 
-
-
     g = rdflib.Graph()
     g.parse("rdf.nt", format='n3')
 
-    import pprint
+    #import pprint
     #for stmt in g:
     #    pprint.pprint(stmt)
-
 
     return rdf_data
